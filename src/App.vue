91d2c8ce0bda06ac6c1f2ef9ec8ef18776e5e2bc
Daria Golova <danalis969@gmail.com>
2023-12-03 18:20:22 +0300

    Сделала домашнюю страницу

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,107 @@
 <template lang="pug">
-  router-view
+  #app
+    v-modal(:title="currentModal?.title", :close="closeModal", v-if="open")
+      <template v-slot>
+        component(v-bind:is="currentModal?.component", :changeModal="changeModal")
+      </template>
+    .page-wrapper.h-full.w-full.flex
+      .header.flex.center-align
+        img.logo(src="@/assets/svg/logo.svg", alt="logo")
+        .log-btn
+          v-button(icon="login", label="Вход", @click.native="openModal")
+      router-view
 </template>
 
 <script>
+import VButton from "@/components/VButton.vue";
+import VModal from "@/components/VModal.vue";
+import LoginModal from "@/pages/home/components/LoginModal.vue";
+import RegistrationModal from "@/pages/home/components/RegistrationModal.vue"
 export default {
   name: "App",
+  components: { VButton, VModal, LoginModal, RegistrationModal },
+  data() {
+    return {
+      open: false,
+      modals: [
+        {
+          component: "login-modal",
+          id: "login",
+          title: "Вход в ваш аккаунт",
+        },
+        {
+          component: "registration-modal",
+          id: "registration",
+          title: "Регистрация",
+        }
+      ],
+      currentModal: {},
+    }
+  },
+  methods: {
+    closeModal() {
+      this.open = false;
+      //this.changeModal(this.$route.path === "/" ? "login" : "out")
+      this.currentModal = this.modals[0]
+    },
+    openModal() {
+      this.open = true;
+    },
+    changeModal(componentName) {
+      this.currentModal = this.modals.find(({id}) => id === componentName);
+    }, 
+  },
+  watch: {
+    "$route.path": {
+      immediate: true,
+      handler(value) {
+        if (value === "/") this.currentModal = this.modals[0];
+      }
+    }
+  }
 }
 </script>
+<style lang="sass" scoped>
+.page-wrapper
+  flex-direction: column
+  position: relative
+
+.header
+  justify-content: space-between
+
+.log-btn
+  width: 144px
+
+@media (min-width: 1366px)
+  .header
+    padding: 40px 0
+  .page-wrapper
+    padding: 0 160px
+
+@media (max-width: 1366px)
+  .header
+    padding: 20px 0
+  .page-wrapper
+    padding: 0 80px
+
+@media (max-width: 768px)
+  .header
+    padding: 20px 0
+  .page-wrapper
+    padding: 0 40px
+
+@media (max-width: 1920px) and (min-width: 360px)
+  .logo
+    width: 180px
+    height: 50px
+
+@media (max-width: 360px)
+  .header
+    padding: 20px 0
+  .page-wrapper
+    padding: 0 20px
+  .logo
+    width: 154px
+    height: 36px
+</style>
 
--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,47 +1,18 @@
 <template lang="pug">
-  .page-wrapper.h-full.w-full.flex
-    .header.flex.center-align
-      img.logo(src="@/assets/svg/logo.svg", alt="logo")
-      .log-btn(v-on:click="openModal")
-        v-button(icon="login", label="Вход")
-    .body.flex.h-full.w-full
-      .heading.flex
-        .h1 Мои заметки
-        .h3 Не забывай о важном, храни его в облаке.
-      img.notes-img(src="@/assets/png/notes.png", alt="notes")
-    login-modal(v-if="open", :closeModal="closeModal")
+  .body.flex.h-full.w-full
+    .heading.flex
+      .h1 Мои заметки
+      .h3 Не забывай о важном, храни его в облаке.
+    img.notes-img(src="@/assets/png/notes.png", alt="notes")
 </template>
 
 <script>
-import VButton from "@/components/VButton.vue";
-import VIcon from "@/components/VIcon.vue";
-import VModal from "@/components/VModal.vue";
-import LoginModal from "@/pages/home/components/LoginModal.vue"
 export default ({
   name: "HomePage",
-  components: { VButton, VIcon, VModal, LoginModal},
-  data() {
-    return {
-      open: false,
-    }
-  },
-  methods: {
-    closeModal() {
-      this.open = false;
-    },
-    openModal() {
-      this.open = true;
-    },
-  }
 })
 </script>
 
 <style scoped lang="sass">
-.page-wrapper
-  flex-direction: column
-  position: relative
-.header
-  justify-content: space-between
 .heading
   flex-direction: column
   row-gap: 40px
@@ -57,10 +28,6 @@ export default ({
   width: 394px
 
 @media (min-width: 1366px)
-  .header
-    padding: 40px 160px
-  .body
-    padding: 0 160px
   .notes-img
     width: 898px
     height: 864px
@@ -69,20 +36,14 @@ export default ({
     justify-content: center
 
 @media (max-width: 1366px)
-  .header
-    padding: 20px 80px
-  .body
-    padding: 0 80px
   .notes-img
     width: 626px
     height: 604px
 
 @media (max-width: 768px)
-  .header
-    padding: 20px 40px
   .body
     flex-direction: column
-    padding: 40px 40px 0 40px
+    padding-top: 40px
     align-items: center
   .heading
     row-gap: 20px
@@ -100,11 +61,9 @@ export default ({
     text-align: center
 
 @media (max-width: 360px)
-  .header
-    padding: 20px 20px
   .body
     flex-direction: column
-    padding: 20px 20px 0 20px
+    padding-top: 20px
     align-items: center
   .heading
     row-gap: 20px
@@ -124,17 +83,5 @@ export default ({
     text-align: center
     font-size: 60px
     line-height: 64px
-
-.log-btn
-  width: 144px
-
-@media (max-width: 1920px) and (min-width: 360px)
-  .logo
-    width: 180px
-    height: 50px
-@media (max-width: 360px)
-  .logo
-    width: 154px
-    height: 36px
 
 </style>
